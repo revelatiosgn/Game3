a229abbba38a8c1d28bc18444d5254b0967c7835
revelatiosgn <revelatiosgn@gmail.com>
2020-10-17 18:49:07 +0300

    running

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,25 +7,45 @@ namespace DSRPG
     [RequireComponent(typeof(Rigidbody))]
     public class Movement : MonoBehaviour
     {
-        public float speed = 10f;
+        public float movementSpeed = 5f;
+        public float rotationSpeed = 5f;
 
         private Rigidbody rb;
+        private Animator animator;
 
         void Awake()
         {
             rb = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
         }
 
         void Update()
         {
             HandleMovement();
+            HandleRotation();
         }
 
         void HandleMovement()
         {
             Vector3 direction = new Vector3(InputHandler.movementInput.x, 0f, InputHandler.movementInput.y);
+            direction.Normalize();
 
-            rb.velocity = direction * speed;
+            rb.velocity = direction * movementSpeed;
+            //rb.velocity = Vector3.SmoothDamp(rb.velocity, direction * speed, ref vl, 0.2f);
+
+            animator.SetFloat("vertical", direction.magnitude, 0.1f, Time.deltaTime);
+        }
+
+        void HandleRotation()
+        {
+            Vector3 direction = rb.velocity.normalized;
+            direction.y = 0f;
+
+            if (direction == Vector3.zero)
+                return;
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
